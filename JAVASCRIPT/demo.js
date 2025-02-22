43bd2c68a7d07877b1c47083cb45f7661389a515
Abhishek Upari <149871626+Abhishek0432@users.noreply.github.com>
2025-02-22 02:58:05 -0500

    Update demo.js

--- JAVASCRIPT/demo.js
+++ JAVASCRIPT/demo.js
@@ -1,7 +1,7 @@
 //Block Scope, Function Scope, Global Scope
 
 //1. Mobile
-/*
+
 {
     var mobile_cost = 10000;
     let mobile_model = 'Oppo';
@@ -240,137 +240,137 @@ function cloth1(){
     console.log(cloth_color,cloth_cost,cloth_size);
 }
 cloth1();
-*/
+
 
     // function one(){
-              //        let str="Sachin";
-              //        let res="";
+                     let str="Sachin";
+                     let res="";
 
-              //        for(let i=str.length-1;i>=0;i--)
-              // {
-              //        res=res+str.charAt(i);
-              // }
-              // document.write(res);
-              // }
-              // one();
+                     for(let i=str.length-1;i>=0;i--)
+              {
+                     res=res+str.charAt(i);
+              }
+              document.write(res);
+              }
+              one();
 
 //write a program to print the vowels in a String
 
-              // function two(){
-              //        let str="Saaeiour";
-              //        let res="";
+              function two(){
+                     let str="Saaeiour";
+                     let res="";
 
-              //        for(let i=0;i<str.length;i++){
-              //               let ch=str.charAt(i);
+                     for(let i=0;i<str.length;i++){
+                            let ch=str.charAt(i);
 
-              //               if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
-              //                      // document.write(ch);
-              //                      console.log(ch);
+                            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+                                   // document.write(ch);
+                                   console.log(ch);
                                    
-              //               }
-              //        }
+                            }
+                     }
 
-              // }
-              // two()
+              }
+              two()
 
 
  //write a program to print the consonants in a String
 
-//  function three(){
-//     let str="Saaeiour";
+ function three(){
+    let str="Saaeiour";
 
-//     for(let i=0;i<str.length;i++){
-//            let ch=str.charAt(i);
-//            if(ch!='a' && ch!='e' && ch!='i' && ch!='o' && ch!='u'){
-//            console.log(ch);
+    for(let i=0;i<str.length;i++){
+           let ch=str.charAt(i);
+           if(ch!='a' && ch!='e' && ch!='i' && ch!='o' && ch!='u'){
+           console.log(ch);
            
-//            }
+           }
 
-//     }
-// }
-// three()
+    }
+}
+three()
 
 
 // Write a program to count number of vowels in a String
 
-    // function four(){
-    //        let str="Saaeiour";
+    function four(){
+           let str="Saaeiour";
            
-    //        let res="";
-    //        for(let i=0;i<str.length;i++){
-    //               let ch=str.charAt(i);
+           let res="";
+           for(let i=0;i<str.length;i++){
+                  let ch=str.charAt(i);
                   
 
-    //               if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
-    //                      // document.write(ch);
-    //                      // console.log(ch);
-    //                      res=res+ch;
+                  if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+                         // document.write(ch);
+                         // console.log(ch);
+                         res=res+ch;
                          
-    //               }
-    //        }
-    //        console.log(res.length);
+                  }
+           }
+           console.log(res.length);
            
-    // }
-    // four()
+    }
+    four()
 
 
 
 // Write a program to count number of vowels in a String
 
-    // function five(){
-    //        let str="Saaeiour";
+    function five(){
+           let str="Saaeiour";
            
-    //        let count=0;
-    //        for(let i=0;i<str.length;i++){
-    //               let ch=str.charAt(i);
+           let count=0;
+           for(let i=0;i<str.length;i++){
+                  let ch=str.charAt(i);
                   
 
-    //               if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
-    //                      // document.write(ch);
-    //                      // console.log(ch);
-    //                      count++;
+                  if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+                         // document.write(ch);
+                         // console.log(ch);
+                         count++;
                          
-    //               }
-    //        }
-    //        console.log(count); 
-    // }
-    // five()
+                  }
+           }
+           console.log(count); 
+    }
+    five()
 
 
 // Write a program to count number of consonants in a String
 
-    // function six(){
-    //        let str="Saaeiour";
-    //        let count=0;
-    //        for(let i=0;i<str.length;i++){
-    //               let ch=str.charAt(i);
+    function six(){
+           let str="Saaeiour";
+           let count=0;
+           for(let i=0;i<str.length;i++){
+                  let ch=str.charAt(i);
 
-    //               if(ch!='a' && ch!='e' && ch!='i'&& ch!='o' && ch!='u'){
-    //                      // document.write(ch);
-    //                      console.log(ch);
-    //                      count++;
-    //               }
-    //        }
-    //        console.log(count); 
-    // }
-    // six()
+                  if(ch!='a' && ch!='e' && ch!='i'&& ch!='o' && ch!='u'){
+                         // document.write(ch);
+                         console.log(ch);
+                         count++;
+                  }
+           }
+           console.log(count); 
+    }
+    six()
 
 
 // function a(){
-//     let str = "hi how are";
-//     let s1 = str.split(" ");
-//     let res = "";
-//     for(let i=s1.length-1;i>=0;i--)
-//     {
-//         res=res+s1[i]+" ";
-//     }
-//     console.log(res);
+    let str = "hi how are";
+    let s1 = str.split(" ");
+    let res = "";
+    for(let i=s1.length-1;i>=0;i--)
+    {
+        res=res+s1[i]+" ";
+    }
+    console.log(res);
     
-// }
-// a()
+}
+a()
 
 
-/*
+
 let str = "hi how are you";
  function b()
  {
@@ -393,269 +393,269 @@ let str = "hi how are you";
  }
  b()
 
- */
 
-//  function a()
-//  {
-//     let str = "amma";
-//     let res = "";
-//     for(let i=str.length-1;i>=0;i--)
-//     {
-//         res=res+str.charAt(i);
-//     }
-//     if(str==res)
-//     {
-//         console.log("palindrome");
+
+ function a()
+ {
+    let str = "amma";
+    let res = "";
+    for(let i=str.length-1;i>=0;i--)
+    {
+        res=res+str.charAt(i);
+    }
+    if(str==res)
+    {
+        console.log("palindrome");
         
-//     }
-//     else{
-//         console.log("not palindromee");
+    }
+    else{
+        console.log("not palindromee");
         
-//     }
-//  }
+    }
+ }
 
-// function reverseStr(str){
-//     //let str="";
-//     let s1=str.split(" ");
-//     let res="";
+function reverseStr(str){
+    //let str="";
+    let s1=str.split(" ");
+    let res="";
 
-//     for(let i=s1.length-1;i>=0;i--){
-//            res=res+s1[i]+" ";
-//     }
-//     document.write(res);
-//     // console.log(res);
+    for(let i=s1.length-1;i>=0;i--){
+           res=res+s1[i]+" ";
+    }
+    document.write(res);
+    // console.log(res);
     
-// }
-// reverseStr("jspider rajajinagar");
+}
+reverseStr("jspider rajajinagar");
 
 
-// function fetchWord1(str){
-//     let s1=str.split(" ");
+function fetchWord1(str){
+    let s1=str.split(" ");
 
-//     for(let i=0;i<s1.length;i++){
-//            reverseWord1(s1[i]+" ");
-//     }
+    for(let i=0;i<s1.length;i++){
+           reverseWord1(s1[i]+" ");
+    }
     
-// }
-// fetchWord1("Hi how are you");
+}
+fetchWord1("Hi how are you");
 
-// function reverseWord1(str2){
-//     let res="";
+function reverseWord1(str2){
+    let res="";
 
-//     for(let i=str2.length-1;i>=0;i--){
-//            res=res+str2.charAt(i);
-//     }
-//     document.write(res);
-// }
-// reverseWord1();
+    for(let i=str2.length-1;i>=0;i--){
+           res=res+str2.charAt(i);
+    }
+    document.write(res);
+}
+reverseWord1();
 
-// function reverseWord(str2){
-//     let res="";
+function reverseWord(str2){
+    let res="";
 
-//     for(let i=str2.length-1;i>=0;i--){
-//            res=res+str2.charAt(i);
-//     }
-//     if(str2==res){
-//            document.write("Palindrome");
-//     }else{
-//            document.write("Not Palindrome");
-//     }
-//     document.write(res);
-// }
-// reverseWord("appa");
+    for(let i=str2.length-1;i>=0;i--){
+           res=res+str2.charAt(i);
+    }
+    if(str2==res){
+           document.write("Palindrome");
+    }else{
+           document.write("Not Palindrome");
+    }
+    document.write(res);
+}
+reverseWord("appa");
 
 //5.charCodeat method used fetch the particular characters
 
-// let str = "jspiders";
-// console.log(str.charCodeAt(0));
+let str = "jspiders";
+console.log(str.charCodeAt(0));
 
 //6.slice method  
-// let str = "jspiders";
-// console.log(str.slice(0,5));
-// console.log(str.slice(-8,-3));
+let str = "jspiders";
+console.log(str.slice(0,5));
+console.log(str.slice(-8,-3));
 
 
 //7.substring
-// let str1 = "jspiders";
-// console.log(str1.substring(0,3));
+let str1 = "jspiders";
+console.log(str1.substring(0,3));
 
 //8.substr
-// let str = "mohan";
-// console.log(str.substr(0,5));
+let str = "mohan";
+console.log(str.substr(0,5));
 
 //9.replace
-// let str = "jspiders rajajinagar"
-// console.log(str.replace("jspiders","qspiders"));
-// console.log(str.replaceAll('j','q'));
+let str = "jspiders rajajinagar"
+console.log(str.replace("jspiders","qspiders"));
+console.log(str.replaceAll('j','q'));
 
 //10.length,trim,trimstart,trimend
-// let str = "               rajajinagar"
-// console.log(str.length);
-// str=str.trimEnd()
-// console.log(str.length);
-// str=str.trimStart();
-// console.log(str.length);
+let str = "               rajajinagar"
+console.log(str.length);
+str=str.trimEnd()
+console.log(str.length);
+str=str.trimStart();
+console.log(str.length);
 
-// str=str.trim()
-// console.log(str.length);
+str=str.trim()
+console.log(str.length);
 
 
 // 11.split
-// let str = "jspiders rajajinagar"
-// let s=str.split(" ");
-// console.log(s);
+let str = "jspiders rajajinagar"
+let s=str.split(" ");
+console.log(s);
 
 
 //pgm to reverse sentence
-// function reverse() {
-//     let s1 = "how are you";
-//     let s2 = s1.split(" ");
-//     let s3 = "";
-//     for(let i=s2.length-1;i>=0;i--)
-//     {
-//         s3=s3+s2[i];
-//     }
-//     console.log(s3);
+function reverse() {
+    let s1 = "how are you";
+    let s2 = s1.split(" ");
+    let s3 = "";
+    for(let i=s2.length-1;i>=0;i--)
+    {
+        s3=s3+s2[i];
+    }
+    console.log(s3);
     
-// }
-// reverse()
+}
+reverse()
 
 //reverse the words in sentence
-// function a() {
-//     let str = "hi helloo";
-//     let res = "";
-//     let arr = str.split(" ");
-//     for(let i=0;i<arr.length;i++){
-//         let word = "";
-//          for(let j = arr[i].length-1;j>=0;j--){
-//             word+=arr[i].charAt(j);
-//          }
-//          res += word + " ";
-//          console.log(res.trim());
+function a() {
+    let str = "hi helloo";
+    let res = "";
+    let arr = str.split(" ");
+    for(let i=0;i<arr.length;i++){
+        let word = "";
+         for(let j = arr[i].length-1;j>=0;j--){
+            word+=arr[i].charAt(j);
+         }
+         res += word + " ";
+         console.log(res.trim());
          
-//     }
-// }
-// a()
+    }
+}
+a()
 
-// let str = "hi hello friends";
-// function b(){
-//     let str1=str.split(" ");
-//     for(let i=0;i<str1.length;i++){
-//         let a = str1[i];
-//         str1[i]=reverse(a);
-//     }
-//     console.log(str1);
+let str = "hi hello friends";
+function b(){
+    let str1=str.split(" ");
+    for(let i=0;i<str1.length;i++){
+        let a = str1[i];
+        str1[i]=reverse(a);
+    }
+    console.log(str1);
     
-// }
-// b()
-// function reverse(str) {
-//     let rev="";
-//     for(let i=str.length-1;i>=0;i--){
-//         rev=rev+str.charAt(i);
-//     }
-// return rev;
-// }
+}
+b()
+function reverse(str) {
+    let rev="";
+    for(let i=str.length-1;i>=0;i--){
+        rev=rev+str.charAt(i);
+    }
+return rev;
+}
 
 
 
 //concat method .used to concat two strings
-// let str = "abhi"
-// let str1 = "shek"
-// let str2 = "upari"
+let str = "abhi"
+let str1 = "shek"
+let str2 = "upari"
 
-// let a=str.concat(" ",str1," ",str2);
-// console.log(a);
+let a=str.concat(" ",str1," ",str2);
+console.log(a);
 
 //includes method if the character is their means give true or else false
 
-// let str = "jspider rajajinagar";
-// console.log(str.includes("j"));
+let str = "jspider rajajinagar";
+console.log(str.includes("j"));
 
 
 //indexof and lastindexof
-// let str = "jspider rajajinagar";
-// console.log(str.indexOf("j"));
-// console.log(str.lastIndexOf("r"));
+let str = "jspider rajajinagar";
+console.log(str.indexOf("j"));
+console.log(str.lastIndexOf("r"));
 
 //padStart and padEnd
-// let str = "5";
-// console.log(str.padStart(3,'0'));
-// console.log(str.padEnd(3,'1'));
+let str = "5";
+console.log(str.padStart(3,'0'));
+console.log(str.padEnd(3,'1'));
 
 //repeat
-// let str = "helloooo";
-// console.log(str.repeat(5));
+let str = "helloooo";
+console.log(str.repeat(5));
 
 //undefined
-// let a;
-// console.log(a);
+let a;
+console.log(a);
 
 // //boolean
-// let str = "foo";
-// let str1 = "foo";
-// console.log(str==str1);
+let str = "foo";
+let str1 = "foo";
+console.log(str==str1);
 
 //bigint
-// let no = 123456n;
-// console.log(typeof no);
-// let no1 = BigInt(12345678);
-// console.log(typeof no1);
+let no = 123456n;
+console.log(typeof no);
+let no1 = BigInt(12345678);
+console.log(typeof no1);
 
-// let no2 = BigInt("12345678");
-// console.log(typeof no2);
+let no2 = BigInt("12345678");
+console.log(typeof no2);
 
  //symbol
-//  let str = "foo";
-//  let str1 = "foo";
-//  console.log(str==str1);  //true
+ let str = "foo";
+ let str1 = "foo";
+ console.log(str==str1);  //true
  
-// let sym=Symbol("foo");
-// let sym1=Symbol("foo");
-// console.log(sym==sym1);
-// console.log(sym);
+let sym=Symbol("foo");
+let sym1=Symbol("foo");
+console.log(sym==sym1);
+console.log(sym);
 
 //equals and strictly equals
-// let a="10";
-// let b = 10;
-// console.log(a==b); //true
-// console.log(a===b);  //false
+let a="10";
+let b = 10;
+console.log(a==b); //true
+console.log(a===b);  //false
 
 
 
-// console.log(typeof null); //object
-// console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
 
 //typecasting:implicit type casting
-// let a="5";
-// let b=a+5;
-// console.log(b);  //55
-// console.log(typeof b);  //string
+let a="5";
+let b=a+5;
+console.log(b);  //55
+console.log(typeof b);  //string
 
-// let x="5";
-// let y = x*5;
-// console.log(y);  //25
-// console.log(typeof y); //number
+let x="5";
+let y = x*5;
+console.log(y);  //25
+console.log(typeof y); //number
 
 
 //Explicit typecasting
-// let m = 100;
-// console.log(typeof m); //number
+let m = 100;
+console.log(typeof m); //number
 
-// let n = String(m);
-// console.log(typeof n); //string
-// console.log(n);
+let n = String(m);
+console.log(typeof n); //string
+console.log(n);
 
 
 //taking dynamic input
 
-// let no=Number(prompt("enter the first value"));
-// console.log(typeof no);  //number
+let no=Number(prompt("enter the first value"));
+console.log(typeof no);  //number
 
-// let no1=Number(prompt("enter the second value"));
-// console.log(typeof no1);  //number
+let no1=Number(prompt("enter the second value"));
+console.log(typeof no1);  //number
 
-// let res=no+no1;
-// console.log(res);
+let res=no+no1;
+console.log(res);
 
 
 
